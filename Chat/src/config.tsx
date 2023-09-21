4d93cbb26e28e988cd9760b63de5573a8ea87700
Hafid Selbi <hselbi@c3r4p5.1337.ma>
2023-09-21 19:16:35 +0100

    hafid: install dependencies

---
+++ Chat/src/config.tsx
@@ -0,0 +1,6 @@
+// routes
+import { PATH_DASHBOARD } from "./routes/paths";
+
+
+// DEFAULT ROOT PATH
+export const DEFAULT_PATH = PATH_DASHBOARD.general.app; // as '/app'
---
+++ Chat/src/routes/index.tsx
@@ -0,0 +1,5 @@
+import { Suspense, lazy } from "react";
+import { Navigate, useRoutes} from "react-router-dom"
+
+// layout
+import DashboardLayout from ""
