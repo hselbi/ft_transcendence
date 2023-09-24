e7db274bf7c63ab75ca920d71f35662a95cf0b64
slo <selbihafid@gmail.com>
2023-09-24 14:04:16 +0100

    ~~~ do not use this ~~~

---
+++ Chat/src/hooks/useLocalStorage.tsx
@@ -0,0 +1,76 @@
+import { useState, useEffect } from 'react';
+
+// ----------------------------------------------------------------------
+
+export default function useLocalStorage(key:any, defaultValue:any) {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+
+    return storedValue === null ? defaultValue : JSON.parse(storedValue);
+  });
+
+  useEffect(() => {
+    const listener = (e:any) => {
+      if (e.storageArea === localStorage && e.key === key) {
+        setValue(JSON.parse(e.newValue));
+      }
+    };
+    window.addEventListener('storage', listener);
+
+    return () => {
+      window.removeEventListener('storage', listener);
+    };
+  }, [key, defaultValue]);
+
+  const setValueInLocalStorage = (newValue:any) => {
+    setValue((currentValue:any) => {
+      const result = typeof newValue === 'function' ? newValue(currentValue) : newValue;
+
+      localStorage.setItem(key, JSON.stringify(result));
+
+      return result;
+    });
+  };
+
+  return [value, setValueInLocalStorage];
+}
+
+
+// ----------------------------------------------------------------------
+/*
+import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+export default function useLocalStorage<T>(key: string, defaultValue: T): [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+
+    return storedValue === null ? defaultValue : JSON.parse(storedValue);
+  });
+
+  useEffect(() => {
+    const listener = (e: StorageEvent) => {
+      if (e.storageArea === localStorage && e.key === key) {
+        setValue(JSON.parse(e.newValue));
+      }
+    };
+    window.addEventListener('storage', listener);
+
+    return () => {
+      window.removeEventListener('storage', listener);
+    };
+  }, [key, defaultValue]);
+
+  const setValueInLocalStorage = (newValue: T | ((prevValue: T) => T)) => {
+    setValue((currentValue) => {
+      const result = typeof newValue === 'function' ? (newValue as (prevValue: T) => T)(currentValue) : newValue;
+
+      localStorage.setItem(key, JSON.stringify(result));
+
+      return result;
+    });
+  };
+
+  return [value, setValueInLocalStorage];
+}
+*/
+// ----------------------------------------------------------------------
--- Chat/src/main.tsx
+++ Chat/src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App.tsx";
+import reportWebVitals from "./reportWebVitals";
+// contexts
+import SettingsProvider from "./contexts/SettingsContext";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SettingsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SettingsProvider>
+  </React.StrictMode>
+);
